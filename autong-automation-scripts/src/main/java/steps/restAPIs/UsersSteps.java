dbf1e5ecac79be75b9a73dd5473e4377d190f80a
Shwetank Vashishtha <autong@Shwetanks-MacBook-Air.local>
2023-08-04 22:38:23 +0530

    Initial commit

---
+++ autong-automation-scripts/src/main/java/steps/restAPIs/UsersSteps.java
@@ -0,0 +1,24 @@
+package steps.restAPIs;
+
+import com.autong.base.Assertions;
+import com.autong.base.RestResources;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+
+public class UsersSteps {
+
+    Response response;
+
+    @When("user executes post call to add test result")
+    public void postAddTestResult() {
+        response = RestResources.get("https://reqres.in/api/users");
+        System.out.println(response.asPrettyString());
+    }
+
+    @Then("user should be able to receive a valid response")
+    public void validateResponse() {
+        Assertions.validateOkResponse(response);
+        Assertions.validateResponseTime(response);
+    }
+}
